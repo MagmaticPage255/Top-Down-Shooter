576c18b5ee3f410ef918594a74e53655be90c478
MagmaticPage255 <cammar83@kingshigh.school.nz>
2024-07-24 13:25:34 +1200

    gave up the visible bullet
    I CANT
    not skibidgty

--- Scenes/player.gd
+++ Scenes/player.gd
@@ -1,11 +1,10 @@
 extends CharacterBody2D
 
 
+@onready var ray_cast_2d = $RayCast2D
 @export var SPEED = 300.0
 @export var ACCELERATION = 20.0
 @export var FRICTION = 10.0
-const bullet = preload("res://Scenes/bullet.tscn")
-@onready var world = get_node('/root/World')
 
 # Get the gravity from the project settings to be synced with RigidBody nodes.
 func _physics_process(delta):
@@ -17,20 +16,24 @@ func _physics_process(delta):
 		velocity = velocity.move_toward(direction * SPEED, ACCELERATION)
 	else:
 		velocity = velocity.move_toward(Vector2.ZERO, FRICTION)
+	
+	
 
-func fire():
-	var bullet = bullet.instantiate()
-	bullet.direction = $Marker2D.global_position - global_position
-	bullet.global_position = $Marker2D.global_position
-	get_tree().get_root().add_child(bullet)
 
 
 
 func _process(delta):
 	look_at(get_global_mouse_position())
 	rotation_degrees += 90
-	
+
 	if Input.is_action_just_pressed("shoot"):
-		fire()
+		shoot()
 
 	move_and_slide()
+
+
+func shoot():
+	$MuzzleFlash.show()
+	$MuzzleFlash/Timer.start()
+	if ray_cast_2d.is_colliding() and ray_cast_2d.get_collider().has_method("kill"):
+		ray_cast_2d.get_collider().kill()
