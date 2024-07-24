e165b688a571752217a853b319869f894a3ee353
MagmaticPage255 <cammar83@kingshigh.school.nz>
2024-07-24 13:55:34 +1200

    SIGMA GRIND
    PLEASE HELP

--- Scenes/player.gd
+++ Scenes/player.gd
@@ -7,6 +7,19 @@ extends CharacterBody2D
 @export var FRICTION = 10.0
 
 # Get the gravity from the project settings to be synced with RigidBody nodes.
+
+var dead = false
+
+func _process(delta):
+	look_at(get_global_mouse_position())
+	rotation_degrees += 90
+
+	if Input.is_action_just_pressed("shoot"):
+		shoot()
+	if dead:
+		return
+	move_and_slide()
+
 func _physics_process(delta):
 
 	# Get the input direction and handle the movement/deceleration.
@@ -16,24 +29,18 @@ func _physics_process(delta):
 		velocity = velocity.move_toward(direction * SPEED, ACCELERATION)
 	else:
 		velocity = velocity.move_toward(Vector2.ZERO, FRICTION)
-	
-	
+	if dead:
+		return
+		
 
-
-
-
-func _process(delta):
-	look_at(get_global_mouse_position())
-	rotation_degrees += 90
-
-	if Input.is_action_just_pressed("shoot"):
-		shoot()
-
-	move_and_slide()
-
+func kill():
+	if dead:
+		return
+	dead = true
 
 func shoot():
 	$MuzzleFlash.show()
 	$MuzzleFlash/Timer.start()
+	$ShootSound
 	if ray_cast_2d.is_colliding() and ray_cast_2d.get_collider().has_method("kill"):
 		ray_cast_2d.get_collider().kill()
