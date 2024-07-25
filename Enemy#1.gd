e98f176e08310e07e45887439c8b6e35be38d3f9
MagmaticPage255 <cammar83@kingshigh.school.nz>
2024-07-25 15:05:07 +1200

    more grinding
    - fixed shooting
    - added enemy
    - broke player collisions

---
+++ Enemy#1.gd
@@ -0,0 +1,22 @@
+extends CharacterBody2D
+
+@onready var ray_cast_2d = $RayCast2D
+
+@export var SPEED = 200.0
+@export var ACCELERATION = 20.0
+@export var FRICTION = 10.0
+@onready var sprite = $AnimatedSprite2D
+@onready var player = get_tree().get_first_node_in_group("player")
+
+
+func _physics_process(delta):
+	var direction_to_player = global_position.direction_to(player.global_position)
+	velocity = direction_to_player * SPEED
+	
+	look_at(player.position)
+	rotation_degrees += 90
+	move_and_slide()
+	
+	
+	if ray_cast_2d.is_colliding() and ray_cast_2d.get_collider() == player:
+		player.kill()
--- Scenes/player.gd
+++ Scenes/player.gd
@@ -6,41 +6,25 @@ extends CharacterBody2D
 @export var ACCELERATION = 20.0
 @export var FRICTION = 10.0
 
-# Get the gravity from the project settings to be synced with RigidBody nodes.
 
-var dead = false
 
 func _process(delta):
+	if Input.is_action_just_pressed("shoot"):
+		shoot()
+
+func _physics_process(delta):
 	look_at(get_global_mouse_position())
 	rotation_degrees += 90
 
-	if Input.is_action_just_pressed("shoot"):
-		shoot()
-	if dead:
-		return
-	move_and_slide()
-
-func _physics_process(delta):
-
-	# Get the input direction and handle the movement/deceleration.
-	# As good practice, you should replace UI actions with custom gameplay actions.
 	var direction = Input.get_vector("left", "right", "up", "down").normalized()
 	if direction:
 		velocity = velocity.move_toward(direction * SPEED, ACCELERATION)
 	else:
 		velocity = velocity.move_toward(Vector2.ZERO, FRICTION)
-	if dead:
-		return
-		
-
-func kill():
-	if dead:
-		return
-	dead = true
+	move_and_slide()
 
 func shoot():
 	$MuzzleFlash.show()
 	$MuzzleFlash/Timer.start()
-	$ShootSound
-	if ray_cast_2d.is_colliding() and ray_cast_2d.get_collider().has_method("kill"):
-		ray_cast_2d.get_collider().kill()
+	$ShootSound.play()
+
