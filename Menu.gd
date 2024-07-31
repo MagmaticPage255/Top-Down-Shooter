07561008713bf8be313823904d3ff67e0ed365ba
MagmaticPage255 <cammar83@kingshigh.school.nz>
2024-07-31 13:51:12 +1200

    more menu
    yuh

--- Menu.gd
+++ Menu.gd
@@ -1,17 +1,10 @@
 extends Control
 
-
-
 func _on_play_pressed():
 	get_tree().change_scene_to_file("res://Scenes/world.tscn")
 
-
-
 func _on_options_pressed():
-	pass # Replace with function body.
-
-
-
+	get_tree().change_scene_to_file("res://Scenes/options_menu.tscn")
 
 func _on_quit_pressed():
 	get_tree().quit()
